3777e9232538586e67c1e129aa138da0fd2f51d0
shakhriyoramirov77-rgb <shakhriyoramirov77@gmail.com>
2025-10-29 08:39:37 +0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import BuyukAvlodSite from './BuyukAvlodSite.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BuyukAvlodSite />
+  </React.StrictMode>,
+)
